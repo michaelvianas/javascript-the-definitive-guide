6e9820411e4b5cd5603029a5e0f1d83098d17e44
michaelvianas <vmichael116@gmail.com>
2023-03-29 09:24:58 -0300

    Uploading chapter 1

---
+++ chapter_1/example-of-js-code-1.js
@@ -0,0 +1,28 @@
+// Anything following double slashes is an English-language comment.
+
+// Read the comments carefully: they explain the JavaScript code.
+
+// A variable is a symbolic name for a value.
+// Variables are declared with the let keyword:
+
+let x;   // This line declares a variable named x
+
+// Values can be assigned to variables with an = sign
+
+x = 0;   // Now the variable x has the value 0
+
+x = 1;   // Numbers
+
+x = 0.01;   // Numbers can be integers or reals.
+
+x = "hello world";   // Strings of text in quotation marks.
+
+x = 'JavaScript';   // Single quote marks also delimit strings.
+
+x = true;   // A Boolean value.
+
+x = false;   // The other Boolean value.
+
+x = null;   // Null is a special value that means "no value."
+
+x = undefined;   // Undefined is another special value like null.
